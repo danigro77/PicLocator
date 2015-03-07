b9f5e80a04861fa25765a3210d6239f9e61901f9
Daniela Grossmann <daniela.grossmann@gmail.com>
2015-03-07 13:52:38 -0800

    add input fields

---
+++ app/controllers/locations_controller.rb
@@ -0,0 +1,6 @@
+class LocationsController < ApplicationController
+
+  def locator
+  end
+
+end
---
+++ app/models/locations.rb
@@ -0,0 +1,8 @@
+class Locations
+
+  def initialize(lat, lon)
+    @lat = lat
+    @lon = lon
+  end
+
+end
