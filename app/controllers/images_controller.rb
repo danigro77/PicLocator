2a79545768079026af3d45bc98a1464e53d14294
Daniela Grossmann <daniela.grossmann@gmail.com>
2015-03-11 16:32:20 -0700

    distance is kind of working

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -4,23 +4,42 @@ class ImagesController < ApplicationController
 
   def index
     client = Instagram.client(:access_token => session[:access_token])
-    radius = (params[:radius].to_i*1000).to_s
     @pictures = []
-    @images = client.location_search(params[:latitude],params[:longitude],radius)
+    @images = client.media_search(params[:latitude],params[:longitude])
     @images.each do |image|
-      picture_array = client.location_recent_media(image['id'])
-      picture_array.each do |picture|
-        @pictures << get_picture(picture,params[:latitude],params[:longitude]) if picture.present?
-      end if picture_array.present?
+      @pictures << get_picture(
+          image,
+          params[:latitude].to_i,
+          params[:longitude].to_i,
+          params[:radius].to_i
+      ) if image.present?
     end
-    render :json => @pictures
+    render :json => @pictures.compact.sort_by(&:distance)
   end
 
   private
 
-  def get_picture(image, o_lat, o_lon)
+  def get_picture(image, o_lat, o_lon, dis=0)
     url = image['images']['standard_resolution']['url']
     lat, lon = image['location']['latitude'], image['location']['longitude']
-    Picture.new(url, o_lat, o_lon, lat, lon)
+    distance = find_distance([o_lat, o_lon], [lat, lon])/1000
+    distance > dis ? nil : Picture.new(url, distance)
+  end
+
+  def find_distance(loc1, loc2) # http://stackoverflow.com/questions/12966638/how-to-calculate-the-distance-between-two-gps-coordinates-without-using-google-m
+    rad_per_deg = Math::PI/180  # PI / 180
+    rkm = 6371                  # Earth radius in kilometers
+    rm = rkm * 1000             # Radius in meters
+
+    dlat_rad = (loc2[0]-loc1[0]) * rad_per_deg  # Delta, converted to rad
+    dlon_rad = (loc2[1]-loc1[1]) * rad_per_deg
+
+    lat1_rad, lon1_rad = loc1.map {|i| i * rad_per_deg }
+    lat2_rad, lon2_rad = loc2.map {|i| i * rad_per_deg }
+
+    a = Math.sin(dlat_rad/2)**2 + Math.cos(lat1_rad) * Math.cos(lat2_rad) * Math.sin(dlon_rad/2)**2
+    c = 2 * Math::atan2(Math::sqrt(a), Math::sqrt(1-a))
+
+    rm * c # Delta in meters
   end
 end
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,22 +1,10 @@
 class Picture
-  def initialize(link, o_lat, o_lon, lat, lon)
+
+  attr_reader :distance
+
+  def initialize(link, distance)
     @url = link
-    @distance = find_distance(o_lat.to_f, o_lon.to_f, lat.to_f, lon.to_f)
+    @distance = distance
   end
 
-  private
-
-  def find_distance(o_lat, o_lon, lat, lon)
-    lat_diff = o_lat >= lat ? (o_lat - lat).abs.to_i : (lat - o_lat).abs.to_i
-    lon_diff = o_lon >= lon ? (o_lon - lon).abs.to_i : (lon - o_lon).abs.to_i
-    if lon_diff == 0 && lat_diff == 0
-      0
-    elsif lon_diff == 0
-      lat_diff
-    elsif lat_diff == 0
-      lon_diff
-    else
-      Math.hypot(lat_diff, lon_diff)
-    end
-  end
 end
